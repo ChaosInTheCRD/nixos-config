a278318df7c869cca396bd4ff3cfce5124396b40
chaosinthecrd <tom@tmlabs.co.uk>
2023-12-12 10:54:58 +0000

    got copilot working

--- pkgs/dev.nix
+++ pkgs/dev.nix
@@ -6,7 +6,11 @@ with pkgs;
     packages = with pkgs; [
       # Command-line tools
       git-crypt cargo yarn protobuf lima goreleaser vulnix protobuf hugo vcluster
-      istioctl go
+      istioctl go scorecard nodejs_21
+
+      ## Tools that I have needed to install in weird circumstances. I don't actually write
+      ## hehehe
+      openjdk maven
     ];
   };
 }
